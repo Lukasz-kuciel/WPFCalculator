240d52928ca4c79b3b62d9db8089e7baa5ba065f
Łukasz Kuciel <lukasz.kuciel1@microsoft.wsei.edu.pl>
2025-04-23 16:53:31 +0200

    basic operations

--- WPFCalculator/MainWindow.xaml.cs
+++ WPFCalculator/MainWindow.xaml.cs
@@ -16,8 +16,74 @@ namespace WPFCalculator;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private double firstNumber = 0;
+    private double secondNumber = 0;
+    private string? symboloperator = null;
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void NumberButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        Button? button = sender as Button;
+        string? digit = button?.Content.ToString();
+        //Display.Content = Display.Content == "0" ? digit : Display.Content + digit;
+        
+        if (Display.Content?.ToString() == "0")
+        {
+            Display.Content = digit;
+        }
+        else
+        {
+            Display.Content += digit;
+        }
+
+    }
+
+    private void OperatorButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        Button? button = sender as Button;
+        string? op = button?.Content.ToString();
+
+        if (op == "=")
+        {
+            secondNumber = double.Parse(Display.Content.ToString());
+            var result = Calcualte();
+            Display.Content = result.ToString();
+            symboloperator = null;
+        }
+        else
+        {
+            firstNumber = double.Parse(Display.Content.ToString());
+            symboloperator = op;
+            Display.Content = "0";
+        }
+    }
+
+    private void DotButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        
+    }
+
+    private void ClearButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        Display.Content = "0";
+    }
+
+    private double Calcualte()
+    {
+        switch (symboloperator)
+        {
+            case "-":
+                return firstNumber - secondNumber;
+            case "+":
+                return firstNumber + secondNumber;
+            case "*":   
+                return firstNumber * secondNumber;
+            case ":":
+                return firstNumber / secondNumber;
+        }
+        return 0;
     }
 }
